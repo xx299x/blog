f2edc340425e470a7d3a88e16b8bdec2775e8c75
Author <Author@admin.com>
2020-09-21 23:45:02 +0800

    Publishing to gh-pages (publish.sh)

--- content/surfingkeys.js
+++ content/surfingkeys.js
@@ -1,9 +1,10 @@
-settings.language = "zh-CN";
-
 // an example to create a new mapping `ctrl-y`
 mapkey('<Ctrl-y>', 'Show me the money', function() {
     Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
 });
+// mapkey('gd','打开下载',function() {
+//     window.open("extension://klibmapgcnoakgokdhnfhlngdphiiinp/ui/index.htm");
+// });
 //将zv与v互换
 //unmap('v') 
 //map('v','zv');
@@ -25,68 +26,73 @@ removeSearchAliasX('d');
 unmap('od');
 removeSearchAliasX('y');
 unmap('oy');
+removeSearchAliasX('b');
+unmap('ob');
 //END
+//下载管理
+mapkey('gd', '#12Open Chrome Downloads', function() {
+        tabOpenLink("chrome://downloadsextension://klibmapgcnoakgokdhnfhlngdphiiinp/ui/index.htm");
 
 addSearchAliasX('g', 'Hong Kong', 'https://www.google.com.hk/search?num=30&newwindow=1&safe=strict&ei=&q=');
-mapkey('og', '#8Open Search with Hong Kong Google', function() {
+mapkey('og', '#8Open  Hong Kong Google', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "g"});
 });
 
 addSearchAliasX('pj', '52PJ search', 'https://www.52pojie.cn/search.php?mod=forum&adv=yes&srchtxt=');
-mapkey('opj', '#8Open Search with 52PJ', function() {
+mapkey('opj', '#8Open  52PJ', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "pj"});
 });
 
 addSearchAliasX('zh', 'ZhiHu search', 'https://www.zhihu.com/search?type=content&q=');
-mapkey('ozh', '#8Open Search with ZhiHu', function() {
+mapkey('ozh', '#8Open  ZhiHu', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "zh"});
 });
 //淘宝
   
 addSearchAliasX('tb', 'TaoBao search', 'https://s.taobao.com/search?q=');
-mapkey('otb', '#8Open Search with Taobao', function() {
+mapkey('otb', '#8Open  Taobao', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "tb"});
 });
 //人人影视
   
 addSearchAliasX('rr', 'RenRen search', 'http://www.91dict.com/words?w=');
-mapkey('orr', '#8Open Open Search with RenRen movie', function() {
+mapkey('orr', '#8Open  RenRen movie', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "rr"});
 });
   
 //forvo
 addSearchAliasX('fo', 'Forvo search', 'https://zh.forvo.com/search/');
-mapkey('ofo', '#8Open Search with Forvo', function() {
+mapkey('ofo', '#8Open  Forvo', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "fo"});
 });
 
 //v2ex
 addSearchAliasX('v2', 'V2EX search', 'https://www.google.com.hk/search?q=site:v2ex.com/t%20');
-mapkey('ov2', '#8Open Search with V2EX', function() {
+mapkey('ov2', '#8Open  V2EX', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "v2"});
 });
   
 //cmd
 addSearchAliasX('cm', 'Cmd search', 'http://www.bathome.net/s/tool/?key=');
-mapkey('ocm', '#8Open Search with Cmd', function() {
+mapkey('ocm', '#8Open  Cmd', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "cm"});
 });
   
 //Python
 addSearchAliasX('py', 'Python search', 'https://python3-cookbook.readthedocs.io/zh_CN/latest/search.html?q=');
-mapkey('opy', '#8Open Search with Python', function() {
+mapkey('opy', '#8Open  Python', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "py"});
 });
 
 //Selenium
 addSearchAliasX('se', 'Selenium search', 'https://selenium-python-zh.readthedocs.io/en/latest/search.html?q=');
-mapkey('ose', '#8Open Search with Selenium', function() {
+mapkey('ose', '#8Open  Selenium', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "se"});
 });
 
 //CSDN
 addSearchAliasX('cs', 'CSDN search', 'https://so.csdn.net/so/search/s.do?q=');
-mapkey('ocs', '#8Open Search with CSDN', function() {
+mapkey('ocs', '#8Open  CSDN', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "cs"});
 });
 
@@ -100,75 +106,75 @@ mapkey('owi', '#8Open Wiki pedia', function() {
   
 //w3xue
 addSearchAliasX('w3', 'W3XUE search', 'https://www.w3xue.com/search.aspx?wd=');
-mapkey('ow3', '#8Open Search with W3XUE', function() {
+mapkey('ow3', '#8Open  W3XUE', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "w3"});
 });
   
 //runoob
 addSearchAliasX('ru', 'Runoob search', 'https://www.runoob.com/?s=');
-mapkey('oru', '#8Open Search with Runoob', function() {
+mapkey('oru', '#8Open  Runoob', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "ru"});
 });
 
 //变量命名
 addSearchAliasX('va', 'Variable search', 'https://unbug.github.io/codelf/#');
-mapkey('ova', '#8Open Search with Variable', function() {
+mapkey('ova', '#8Open  Variable', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "va"});
 });
 
 
 //变量命名
 addSearchAliasX('li', 'Linux Command search', 'http://man.linuxde.net/');
-mapkey('oli', '#8Open Search with Linux Command', function() {
+mapkey('oli', '#8Open  Linux Command', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "li"});
 });
   
 //WeChat
 addSearchAliasX('wx', 'WeChat search', 'https://weixin.sogou.com/weixin?type=2&query=');
-mapkey('owx', '#8Open Search with WeChat', function() {
+mapkey('owx', '#8Open  WeChat', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "wx"});
 });
   
 //微博
 addSearchAliasX('wb', 'WeiBo search', 'https://s.weibo.com/weibo/');
-mapkey('owb', '#8Open Search with WeiBo', function() {
+mapkey('owb', '#8Open  WeiBo', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "wb"});
 });
 
 //GitHub
 addSearchAliasX('hu', 'GitHub search', 'https://github.com/search?q=');
-mapkey('ohu', '#8Open Search with GitHub', function() {
+mapkey('ohu', '#8Open  GitHub', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "hu"});
 });
 
 //gif 动图
 addSearchAliasX('dt', 'GIF search', 'https://www.soogif.com/search/1/');
-mapkey('odt', '#8Open Search with GIF', function() {
+mapkey('odt', '#8Open  GIF', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "dt"});
 });
 
 //逗比图片
 addSearchAliasX('dq', '逗比图片 search', 'http://www.dbbqb.com/s?w=');
-mapkey('odq', '#8Open Search with 逗比图片', function() {
+mapkey('odq', '#8Open  逗比图片', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "dq"});
 });
 
 //用药助手
 addSearchAliasX('yy', '用药助手 search', 'http://drugs.dxy.cn/search/drug.htm?keyword=');
-mapkey('oyy', '#8Open Search with 用药助手', function() {
+mapkey('oyy', '#8Open  用药助手', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "yy"});
 });
   
 //查企业
 addSearchAliasX('qy', '查企业 search', 'https://www.qichacha.com/search?key=');
-mapkey('oqy', '#8Open Search with 查企业', function() {
+mapkey('oqy', '#8Open  查企业', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "qy"});
 });
   
   
 //w3cscool
 addSearchAliasX('sc', 'W3cSchool search', 'https://www.w3cschool.cn/search?w=');
-mapkey('osc', '#8Open Search with W3cSchool', function() {
+mapkey('osc', '#8Open  W3cSchool', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "sc"});
 });
   
@@ -176,118 +182,198 @@ mapkey('osc', '#8Open Search with W3cSchool', function() {
   
 //欧路词典
 addSearchAliasX('ol', '欧路词典 search', 'https://dict.eudic.net/dicts/en/');
-mapkey('ool', '#8Open Search with 欧路词典', function() {
+mapkey('ool', '#8Open  欧路词典', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "ol"});
 });
   
 //Tldr
 addSearchAliasX('tl', 'tldr search', 'https://tldr.ostera.io/');
-mapkey('otl', '#8Open Search with tldr', function() {
+mapkey('otl', '#8Open  tldr', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "tl"});
 });
 
 //思否
 addSearchAliasX('sf', '思否 search', 'https://segmentfault.com/search?q=');
-mapkey('osf', '#8Open Search with 思否', function() {
+mapkey('osf', '#8Open  思否', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "sf"});
 });
   
 //bilibili
-addSearchAliasX('il', 'bilibili search', 'https://search.bilibili.com/all?keyword=');
-mapkey('oil', '#8Open Search with bilibili', function() {
-    Front.openOmnibar({type: "SearchEngine", extra: "il"});
+addSearchAliasX('ll', 'bilibili search', 'https://search.bilibili.com/all?keyword=');
+mapkey('oll', '#8Open  bilibili', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "ll"});
 });
 //wikihow-zh
 addSearchAliasX('whz', 'wikihow search', 'https://zh.wikihow.com/wikiHowTo?search=');
-mapkey('owhz', '#8Open Search with wikihow', function() {
+mapkey('owhz', '#8Open  wikihow', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "whz"});
 });
 //Z-Library 
 addSearchAliasX('zl', 'Z-Library  search', 'https://b-ok.cc/s/?q=');
-mapkey('ozl', '#8Open Search with Z-Library', function() {
+mapkey('ozl', '#8Open  Z-Library', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "zl"});
 });
 //SEO中国站长
 addSearchAliasX('zz', 'Chinaz search', 'https://seo.chinaz.com/?q=');
-mapkey('ozz', '#8Open Search with Chinaz', function() {
+mapkey('ozz', '#8Open  Chinaz', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "zz"});
 });
 
 //wikihow
 addSearchAliasX('whe', 'wikihow search', 'https://www.wikihow.com/wikiHowTo?search=');
-mapkey('owhe', '#8Open Search with wikihow', function() {
+mapkey('owhe', '#8Open  wikihow', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "whe"});
 });
 
 //youtube
 addSearchAliasX('yu', 'Youtube search', 'https://www.youtube.com/results?search_query=');
-mapkey('oyu', '#8Open Search with Youtube', function() {
+mapkey('oyu', '#8Open  Youtube', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "yu"});
 });
 
 //youku
 addSearchAliasX('yk', 'YouKu search', 'https://so.youku.com/search_video/q_');
-mapkey('oyk', '#8Open Search with youku', function() {
+mapkey('oyk', '#8Open  youku', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "yk"});
 });
   
 //语法网
 addSearchAliasX('yf', '英语语法网 search', 'https://www.google.com.hk/search?num=30&newwindow=1&safe=strict&ei=&q=site%3Awww.yygrammar.com+');
-mapkey('oyf', '#8Open Search with 英语语法网', function() {
+mapkey('oyf', '#8Open  英语语法网', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "yf"});
 });
 
 //emacs论坛
 addSearchAliasX('em', 'emacs community search', 'https://emacs-china.org/search?q=');
-mapkey('oem', '#8Open Search with emacs community', function() {
+mapkey('oem', '#8Open  emacs community', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "em"});
 });
 
 //chocolatey
 addSearchAliasX('ch', 'Chocolatey community search', 'https://chocolatey.org/packages?q=');
-mapkey('och', '#8Open Search with Chocolatey community', function() {
+mapkey('och', '#8Open  Chocolatey community', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "ch"});
 });
 
 //网盘
 addSearchAliasX('wp', 'WanPan elf search', 'https://www.yunpanjingling.com/search/');
-mapkey('owp', '#8Open Search with WanPan elf', function() {
+mapkey('owp', '#8Open  WanPan elf', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "wp"});
 });
 
 //豆瓣
 addSearchAliasX('db', 'DouBan elf search', 'https://www.douban.com/search?q=');
-mapkey('odb', '#8Open Search with DouBan', function() {
+mapkey('odb', '#8Open  DouBan', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "db"});
 });
 
 
 //R文档
 addSearchAliasX('rd', 'R document search', 'https://www.google.com.hk/search?q=site:www.rdocumentation.org ');
-mapkey('ord', '#8Open Search with R document', function() {
+mapkey('ord', '#8Open  R document', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "rd"});
 });
 //有道
 addSearchAliasX('yd', 'YouDao dictionary', 'http://dict.youdao.com/m/search?q=');
-mapkey('oyd', '#8Open Search with YouDao dictionary', function() {
+mapkey('oyd', '#8Open  YouDao dictionary', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "yd"});
 });
 // 词不离句
 addSearchAliasX('cj', '词句 document search', 'http://www.bj3530979798.com/sr.jsp?skeyword=');
-mapkey('ocj', '#8Open Search with 词句 document', function() {
+mapkey('ocj', '#8Open  词句 document', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "cj"});
 });
-// 词不离句
-addSearchAliasX('tt', '头条 search', 'https://www.toutiao.com/search/?keyword=');
-mapkey('ott', '#8Open Search with 头条', function() {
+// 搜索头条
+addSearchAliasX('to', '头条 search', 'https://www.toutiao.com/search/?keyword=');
+mapkey('oto', '#8Open  头条', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "to"});
+});
+
+// 搜索twitter
+addSearchAliasX('tt', 'twitter search', 'https://twitter.com/search?q=');
+mapkey('ott', '#8Open  twitter', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "tt"});
 });
 
-// 词不离句
+// Google cache
 addSearchAliasX('ca', 'Google cache search', 'http://webcache.googleusercontent.com/search?q=cache:');
-mapkey('oca', '#8Open Search with Google cache', function() {
+mapkey('oca', '#8Open  Google cache', function() {
     Front.openOmnibar({type: "SearchEngine", extra: "ca"});
 });
+// Google search
+addSearchAliasX('rg', 'emacs document search', 'https://www.google.com.hk/search?q=site:www.gnu.org+OR+site:orgmode.org ');
+mapkey('org', '#8Open  emacs document', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "rg"});
+});
+// 视频解析
+addSearchAliasX('vi', 'VIP解析 search', 'http://jx.598110.com/?url=');
+mapkey('ovi', '#8Open  VIP解析 document', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "vi"});
+});
+// 少数派
+addSearchAliasX('ss', '少数派搜索 search', 'https://www.google.com.hk/search?q=site:sspai.com ');
+mapkey('oss', '#8Open  少数派搜索 document', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "ss"});
+});
+// Boss直聘
+addSearchAliasX('os', 'Boss直聘 search', 'https://www.zhipin.com/c101210100/ka=sel-city-101210100&/?query=');
+mapkey('oos', '#8Open Boss直聘搜索 ', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "os"});
+});
+
+// anki
+addSearchAliasX('an', 'Anki search', 'https://www.google.com.hk/search?q=site:ankiweb.net ');
+mapkey('oan', '#8Open  Anki Deck', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "an"});
+});
+// anki
+addSearchAliasX('la', '恋爱话术 search', 'http://dialog.360lyq.com/index.html#/search?text=');
+mapkey('ola', '#8Open  恋爱话术 Deck', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "la"});
+});
+
+addSearchAliasX('wj', '问卷星 search', 'https://www.google.com.hk/search?q=site:wjx.cn ');
+mapkey('owj', '#8Open  问卷星 Deck', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "wj"});
+});
+
+addSearchAliasX('b', 'dogedoge search', 'https://www.dogedoge.com/results?q=');
+mapkey('ob', '#8Open  dogedoge Deck', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: 'b'});
+});
+
+addSearchAliasX('do', 'dogedoge search', 'https://www.baidu.com/s?wd=');
+mapkey('odo', '#8Open  dogedoge Deck', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: 'do'});
+});
+
+addSearchAliasX('ph', 'phrase search', 'https://www.google.com.hk/search?q=site:www.ldoceonline.com OR site:www.macmillandictionary.com OR site:thefreedictionary.com ');
+mapkey('oph', '#8phrase', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: "ph"});
+});
+
+addSearchAliasX('df', 'Thefreedictionary', 'https://idioms.thefreedictionary.com/ ');
+mapkey('odf', '#8phrase', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: 'df'});
+});
+
+addSearchAliasX('dl', '大力搜索','https://www.dalipan.com/search?keyword=');
+mapkey('odl', '#8Open 大力搜索', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: 'dl'});
+});
+
+addSearchAliasX('zb', '真不卡影院','https://www.zhenbuka.com/vodsearch/-------------/?wd=');
+mapkey('ozb', '#8Open 真不卡影院', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: 'zb'});
+});
+
+
+
+addSearchAliasX('ys', '问医生','https://www.google.com.hk/search?q=site:haodf.com OR site:youlai.cn OR site:dxy.com ');
+mapkey('oys', '#8Open 问医生', function() {
+    Front.openOmnibar({type: "SearchEngine", extra: 'ys'});
+});
+
 ////////// 翻译设置
 Front.registerInlineQuery({
         url: "https://api.shanbay.com/bdc/search/?word=",
