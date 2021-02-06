9a9323848ef4ae67ba3da1798e5d24b91d9577c1
Elliott <xx299x@gmail.com>
2021-02-06 21:16:06 +0800

    Publishing to gh-pages (publish.sh)

--- publish.bat
+++ publish.bat
@@ -14,7 +14,9 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public
+git add --all
+git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Pushing to github"
 git push --all -f
